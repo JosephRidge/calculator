d625aacfe8adcb5c0eb17cf73998e2829ea417c4
JosephRidge <josephridge@localhost.localdomain>
2023-02-15 21:54:14 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# What is JS 
+- Its a dynamic language, changes on runtime
+- tun on a JS Engine eg V8 for chrome, Spider Monkey for Firefox and NodeJS for non-browsers
+
+## Variables
+ - Data types ( primitves & Refrerences) 
+ 
+## Mathematical Operations
+## loops 
---
+++ assets/script/index.js
@@ -0,0 +1,29 @@
+/**
+ * take 2 values
+ * take target operation
+ * imoplement or get resukt
+ * 
+ */
+
+
+let value = document.getElementsByClassName("element")
+
+function setOperator(operator){
+document.getElementById("operator") = open;
+}
+
+function addition(){
+
+}
+
+function setValue(value){
+ console.log("== = = = = ", value)
+}
+
+function setFirstValue(value){
+
+}
+
+function setSecondValue(value){
+
+}
